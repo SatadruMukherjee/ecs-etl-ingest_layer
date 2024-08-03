a4cb880e57b7251573621efdbbc8aa5b055dea11
SatadruMukherjee <48749591+SatadruMukherjee@users.noreply.github.com>
2024-08-03 18:26:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask,request
+
+import json
+from json import dumps
+from kafka import KafkaProducer
+
+topic_name='captured_data'
+
+producer = KafkaProducer(
+    bootstrap_servers='factual-mallard-9864-us1-kafka.upstash.io:9092',
+    sasl_mechanism='SCRAM-SHA-256',
+    security_protocol='SASL_SSL',
+    sasl_plain_username='ZmFjdHVhbC1tYWxsYXJkLTk4NjQkBCHEsF8gE1rBGXODGoPHR-rRkzBeb6EiuaY',
+    sasl_plain_password='NjUwOGU0MjgtMjI3NC00ZjlmLWE2ZTktZjY2ZDJmYTA4N2Zk',
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+@app.route('/publish_data',methods=['POST'])
+def messagepasser():
+    try:
+        message=request.json
+        print("Sending kafka message")
+        resp = producer.send(topic_name, value=message)
+        print(resp)
+        print("Sent message to kafka")
+        return "Success"
+    except:
+        return "Error"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=50000)
